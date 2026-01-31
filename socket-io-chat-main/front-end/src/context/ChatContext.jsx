f9d402adb38e1d0acee042cdcfafc457641cf756
mouad <mouadinfo30@gmail.com>
2026-02-01 00:06:05 +0100

    Implementing real time messages exchange

--- socket-io-chat-main/front-end/src/context/ChatContext.jsx
+++ socket-io-chat-main/front-end/src/context/ChatContext.jsx
@@ -42,6 +42,28 @@ export const ChatContextProvider = ({ children, user }) => {
   }, [socket]);
 
   useEffect(() => {
+    if (socket === null) return;
+
+    const recipientId = currentChat?.members?.find((id) => id !== user?._id);
+
+    socket.emit("sendMessage", { ...newMessage, recipientId });
+  }, [newMessage]);
+
+  useEffect(() => {
+    if (socket === null) return;
+
+    socket.on("getMessage", (response) => {
+      if (currentChat?._id !== response.chatId) return;
+
+      setMessages((prev) => [...prev, response]);
+    });
+
+    return () => {
+      socket.off("getMessage");
+    };
+  }, [socket, currentChat]);
+
+  useEffect(() => {
     const getUsers = async () => {
       const response = await getRequest(`http://localhost:5000/api/users`);
 
